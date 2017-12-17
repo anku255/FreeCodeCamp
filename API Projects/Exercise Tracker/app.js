684b1428327102ba4e77f0bac20df5bb5b72a329
Ankit Tiwari <ankitt255@gmail.com>
2017-12-17 12:28:38 +0530

    Exercise Tracker: Add error handling middleware

--- API Projects/Exercise Tracker/app.js
+++ API Projects/Exercise Tracker/app.js
@@ -25,6 +25,32 @@ app.use(express.static(__dirname + '/public'));
 // handle routes
 app.use('/', routes);
 
+// Not found middleware
+app.use((req, res, next) => {
+  return next({ status: 404, message: 'not found' });
+});
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage;
+
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400; // bad request
+    const keys = Object.keys(err.errors);
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message;
+  } else {
+    // generic or custom error
+    errCode = err.status || 500;
+    errMessage = err.message || 'Internal Server Error';
+  }
+  res
+    .status(errCode)
+    .type('txt')
+    .send(errMessage);
+});
+
 // setup port
 app.set('port', process.env.PORT || 3000);
 
